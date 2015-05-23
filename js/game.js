abd7cf38239bd0d57b18ad2e9142716de76a9c67
Patrick Hurley <phurley@gmail.com>
2015-05-23 16:12:36 -0400

    phaser test

--- js/game.js
+++ js/game.js
@@ -5,4 +5,19 @@ $(function () {
         message: "It works!!!"
     });
   });
+
+        var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create });
+
+        function preload () {
+
+            game.load.image('logo', 'img/phaser.png');
+
+        }
+
+        function create () {
+
+            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+            logo.anchor.setTo(0.5, 0.5);
+
+        }
 });
